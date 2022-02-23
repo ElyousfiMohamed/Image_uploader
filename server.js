5f1eb238d4799cd140a0e854ffcd5aa3f13f5ab1
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-02-23 22:48:44 +0100

    View Engine

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const app = express()
 
+const hbs = require('express-handlebars')
 const path = require ('path')
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'public')))
 
+//setup view engine
+app.set('view engine','hbs')
+app.engine('hbs',hbs.engine({
+    extname:'hbs',
+    defaultView:'default',
+    layoutsDir: path.join(__dirname,'views'),
+    partialsDir: path.join(__dirname,'views/partials')
+}))
+
 //routes 
 app.get('/',(request,response) => {
-    response.send("<h3>Express server</h3>")
+    response.render('main')
 })
 
 app.listen(3000,() => console.log('Server is started on port 3000'))
