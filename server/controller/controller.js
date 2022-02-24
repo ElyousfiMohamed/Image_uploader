dded854a89c62a1c06d8ee1970979888224bc4ef
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-02-24 14:16:05 +0100

    Building Schema for the documents and store the data in Mongo DB

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,11 +1,12 @@
-const res = require("express/lib/response")
+const UploadModel = require('../schema/schema');
+const fs = require("fs")
 
 exports.home = (request,response) => {
     response.render('main')
 }
 
-exports.uploads = (request,response,next) => {
-    const files = request.files
+exports.uploads = (req,res,next) => {
+    const files = req.files
 
     if(!files) {
         const error = new Error('Please choose files')
@@ -13,5 +14,43 @@ exports.uploads = (request,response,next) => {
         return next(error)
     }
 
-    response.json(files)
+     // convert images to base64 encoding
+     let imgArray = files.map((file) => {
+        let img = fs.readFileSync(file.path)
+
+        return encode_image = img.toString('base64')
+    })
+
+    let result = imgArray.map((src, index) => {
+
+        let finalImg = {
+            filename : files[index].originalname,
+            contentType : files[index].mimetype,
+            imageBase64 : src
+        }
+
+        let newUpload = new UploadModel(finalImg);
+
+        return newUpload
+                .save()
+                .then(() => {
+                    return { msg : `${files[index].originalname} Uploaded Successfully...!`}
+                })
+                .catch(error =>{
+                    if(error){
+                        if(error.name === 'MongoError' && error.code === 11000){
+                            return Promise.reject({ error : `Duplicate ${files[index].originalname}. File Already exists! `});
+                        }
+                        return Promise.reject({ error : error.message || `Cannot Upload ${files[index].originalname} Something Missing!`})
+                    }
+                })
+    })
+
+    Promise.all(result)
+        .then( msg => {
+            res.redirect('/')
+        })
+        .catch(err =>{
+            res.json(err);
+        })
 }
